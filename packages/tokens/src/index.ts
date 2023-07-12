136381e316a3364bcd8a98a242bafc0c9f1099ec
primodeckers <primodeckers@hotmail.com>
2023-07-12 20:15:57 -0300

    first commit

--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -1,1 +1,8 @@
 export * from './colors'
+export * from './radii'
+
+export * from './fonts'
+export * from './font-sizes'
+export * from './font-weights'
+export * from './line-heights'
+export * from './space'
