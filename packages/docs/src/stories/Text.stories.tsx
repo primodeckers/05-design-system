8cc36d33b0a88bc62b2a03eb4e106b32f0076a35
primodeckers <primodeckers@hotmail.com>
2023-08-11 19:49:54 -0300

    first commit

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,42 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Tipography/Text',
+  component: Text,
+  tags: ['autodocs'],
+  args: {
+    size: 'md',
+    children:
+      'lorem ipsum dolor sit amet, consectetur adipiscing lorem ipsum dolor',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
