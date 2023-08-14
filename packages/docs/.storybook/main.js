d73b23fdb8ae1fa2dd27f20d23d00b0d9b88e0ef
primodeckers <primodeckers@hotmail.com>
2023-08-14 20:11:58 -0300

    github actions gain

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -10,6 +10,15 @@ const config = {
   framework: {
     name: "@storybook/react-vite",
     options: {}
+  },
+  features: {
+    storyStoreV7: true
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/05-design-system'
+    }
+    return config
   }
 };
 export default config;
